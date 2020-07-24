09f42a4b7500d96d913b5c46a0cea78de4be5f44
Ronak Malik <ronakmalik4242@gmail.com>
2020-07-23 19:45:38 -0500

    Add ping request system and blocking method waiting for ping

--- src/main/java/ml/dent/net/ControllerNetworkClient.java
+++ src/main/java/ml/dent/net/ControllerNetworkClient.java
@@ -2,12 +2,26 @@ package ml.dent.net;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import ml.dent.util.DaemonThreadFactory;
 import ml.dent.util.Markers;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ControllerNetworkClient extends SimpleNetworkClient {
 
+    private ScheduledThreadPoolExecutor pingScheduler = new ScheduledThreadPoolExecutor(1, new DaemonThreadFactory());
+    private ScheduledFuture<?>          pingFuture;
+
     public ControllerNetworkClient(String host, int port) {
         super(host, port, '0');
+        pingScheduler.setRemoveOnCancelPolicy(true);
     }
 
     @Override
@@ -64,11 +78,48 @@ public class ControllerNetworkClient extends SimpleNetworkClient {
         flush();
     }
 
-    private class ControllerInboundHandler extends ChannelInboundHandlerAdapter {
+    private BooleanProperty isMillAccessible = new SimpleBooleanProperty(false);
 
+    public ReadOnlyBooleanProperty millAccessProperty() {
+        return BooleanProperty.readOnlyBooleanProperty(isMillAccessible);
+    }
+
+    private BooleanProperty pingSent = new SimpleBooleanProperty(false);
+
+    public boolean awaitNextPing(long timeout) throws InterruptedException {
+        CountDownLatch wait = new CountDownLatch(1);
+        InvalidationListener pingChange = (listener) -> {
+            if (!pingSent.get()) {
+                wait.countDown();
+            }
+        };
+        pingSent.addListener(pingChange);
+        boolean recvPing = wait.await(timeout, TimeUnit.MILLISECONDS);
+        pingSent.removeListener(pingChange);
+        return recvPing;
+    }
+
+    private class ControllerInboundHandler extends ChannelInboundHandlerAdapter {
         @Override
         public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            pingFuture = pingScheduler.scheduleAtFixedRate(() -> {
+                if (pingSent.get()) {
+                    isMillAccessible.set(false);
+                } else {
+                    if (isConnectionActive()) {
+                        writeAndFlush(Markers.PING_REQUEST);
+                        pingSent.set(true);
+                    }
+                }
+            }, 0L, 5L, TimeUnit.SECONDS);
+
             super.channelActive(ctx);
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            pingFuture.cancel(true);
+            super.channelInactive(ctx);
         }
 
         @Override
@@ -80,6 +131,12 @@ public class ControllerNetworkClient extends SimpleNetworkClient {
             for (byte aByte : bytes) {
                 if (aByte == Markers.PING_REQUEST) {
                     writeAndFlush(Markers.PING_RESPONSE);
+                }
+                if (aByte == Markers.PING_RESPONSE) {
+                    if (pingSent.get()) {
+                        pingSent.set(false);
+                    }
+                    isMillAccessible.set(true);
                 }
             }
 
